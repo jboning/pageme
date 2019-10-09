8ba15c41555835c01878711294bffda5b6b5ea85
Josiah Boning <jboning@gmail.com>
2019-10-08 22:44:41 -0700

    Add real(ish) icons
    "!" in Fira Code Medium. BAMRU logo colors (#000079 on #ffcd00). 15%
    padding on notification icon.

--- app/src/main/java/name/jboning/pageme/SmsReceiver.java
+++ app/src/main/java/name/jboning/pageme/SmsReceiver.java
@@ -57,7 +57,7 @@ public class SmsReceiver extends BroadcastReceiver {
 
             Notification.Builder notificationBuilder =
                     new Notification.Builder(context, ALERT_CHANNEL_ID)
-                            .setSmallIcon(R.drawable.ic_launcher_foreground)
+                            .setSmallIcon(R.drawable.ic_notif)
                             .setContentTitle("Incoming alert!")
                             .setContentText(msg.getBody())
                             .setCategory(Notification.CATEGORY_ALARM)
